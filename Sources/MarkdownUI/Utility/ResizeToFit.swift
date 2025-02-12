1413f5b64da704236823487a9592ba30529428b5
Chad Weider <cweider@oofn.net>
2025-02-11 21:31:58 -0800

    chore: address concurrency warning on `onPreferenceChange`
    In the SDK's release with Xcode 16.2, Apple marked this closure
    `@Sendable`, but *not* `@MainActor`. It's not clear if this is
    intentional or if it may be revised later. The official
    recommendation appears to be capturing the `Binding` and writing
    to it (`Binding` is asserted to be `Sendable`).
    https://stackoverflow.com/questions/79209071/xcode-16-2-beta-2-renders-onpreferencechange-unusable

--- Sources/MarkdownUI/Utility/ResizeToFit.swift
+++ Sources/MarkdownUI/Utility/ResizeToFit.swift
@@ -34,8 +34,8 @@ private struct ResizeToFit1<Content>: View where Content: View {
         .preference(key: SizePreference.self, value: size)
     }
     .frame(width: size?.width, height: size?.height)
-    .onPreferenceChange(SizePreference.self) { size in
-      self.size = size
+    .onPreferenceChange(SizePreference.self) { [$size] size in
+      $size.wrappedValue = size
     }
   }
 
--- Sources/MarkdownUI/Views/Blocks/NumberedListView.swift
+++ Sources/MarkdownUI/Views/Blocks/NumberedListView.swift
@@ -41,8 +41,8 @@ struct NumberedListView: View {
     )
     .environment(\.listLevel, self.listLevel + 1)
     .environment(\.tightSpacingEnabled, self.isTight)
-    .onColumnWidthChange { columnWidths in
-      self.markerWidth = columnWidths[0]
+    .onColumnWidthChange { [$markerWidth] columnWidths in
+      $markerWidth.wrappedValue = columnWidths[0]
     }
   }
 }
